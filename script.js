b1bb6bb8f85d1aa113eee81a0f1495c416cb67cc
Asmahan97 <asmahanhmohamed@gmail.com>
2021-12-04 23:21:37 +0000

    1 file modified

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ function greetUser(str){
  }
  
  console.log(greetUser("test")); 
+
+ //***Write a function which returns the type of the argument we pass in***
+
+function checkType(arg) {
+  return typeof(arg)
+}
+
+console.log(checkType("hey"));
+console.log(checkType([11, 44, 777, 9999])); //Arrays are considered objects in JS so object is returned
+console.log(checkType(true))
+console.log(checkType({name: "Asmahan"}));;
+console.log(checkType(null)); //null always returns obj when using typeof opertor
+console.log(checkType(undefined)); 
