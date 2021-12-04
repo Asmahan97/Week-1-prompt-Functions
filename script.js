ba963a6a5962aeaa789bd48ab8f6987402131841
Asmahan97 <asmahanhmohamed@gmail.com>
2021-12-04 23:21:05 +0000

    1 file modified

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('hello!')
+//***Write a function which greets the user***
+
+//write function that takes a string
+//return greeting + str
+
+function greetUser(str){
+  return ("Hello, " + str)
+ }
+ 
+ console.log(greetUser("test")); 
